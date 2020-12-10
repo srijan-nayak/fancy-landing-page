2a060b60a2a64e5a108cfde7a2a735a1278aaf4e
Srijan Nayak <srijannayak0@gmail.com>
2020-12-10 19:35:18 +0530

    feat: Make nav menu expandable

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("Script connected");
+const navbarExpandButton = document.querySelector(".nav-bar__hamburger");
+const navbarLinks = document.querySelector(".nav-bar__links");
+
+navbarExpandButton.addEventListener("click", () => {
+  navbarExpandButton.classList.toggle("nav-bar__hamburger--active");
+  navbarLinks.classList.toggle("nav-bar__links--expanded");
+});
