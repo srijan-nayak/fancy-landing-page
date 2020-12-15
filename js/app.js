ba0a4a8f06fa8307e63c5711c5da82e1ea608995
Srijan Nayak <srijannayak0@gmail.com>
2020-12-15 15:05:55 +0530

    feat: Toggle active state for section in view

--- js/app.js
+++ js/app.js
@@ -33,6 +33,26 @@ navbarExpandButton.addEventListener("click", () => {
 // -----------------------------------------------
 navbarLinks.addEventListener("click", (e) => {
   if (e.target.classList.contains("nav-bar__link")) {
+    navbarExpandButton.classList.remove("nav-bar__hamburger--active");
     navbarLinks.classList.remove("nav-bar__links--expanded");
   }
+});
+
+// --------------------------------------------------
+// toggle active state for a section when in viewport
+// --------------------------------------------------
+document.addEventListener("scroll", () => {
+  setTimeout(() => {
+    const windowMidPoint = window.innerHeight / 2;
+    pageSections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const sectionBottom = section.getBoundingClientRect().bottom;
+      // check if section is taking up the major area in viewport
+      if (sectionTop < windowMidPoint && sectionBottom >= windowMidPoint) {
+        section.classList.add("page-section--active");
+      } else {
+        section.classList.remove("page-section--active");
+      }
+    });
+  }, 300);
 });
